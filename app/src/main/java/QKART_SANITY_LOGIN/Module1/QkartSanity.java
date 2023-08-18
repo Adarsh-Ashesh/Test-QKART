28ba8980fe4c8aafe49f0f4edb852b8aa301172f
CrioUser <crio-user@criodo.com>
2023-08-18 15:08:04 +0530

    “ME_QKART_QA_V2_MODULE_TEST_AUTOMATION3”

--- app/src/main/java/QKART_SANITY_LOGIN/Module1/QkartSanity.java
+++ app/src/main/java/QKART_SANITY_LOGIN/Module1/QkartSanity.java
@@ -459,30 +459,30 @@ public class QkartSanity {
 
         try {
             // Execute Test Case 1
-            // totalTests += 1;
-            // status = TestCase01(driver);
-            // if (status) {
-            //     passedTests += 1;
-            // }
+            totalTests += 1;
+            status = TestCase01(driver);
+            if (status) {
+                passedTests += 1;
+            }
 
-            // System.out.println("");
+            System.out.println("");
 
             // // Execute Test Case 2
-            // totalTests += 1;
-            // status = TestCase02(driver);
-            // if (status) {
-            //     passedTests += 1;
-            // }
+            totalTests += 1;
+            status = TestCase02(driver);
+            if (status) {
+                passedTests += 1;
+            }
 
-            // System.out.println("");
+            System.out.println("");
             // Execute Test Case 3
-            // totalTests += 1;
-            // status = TestCase03(driver);
-            // if (status) {
-            // passedTests += 1;
-            // }
+            totalTests += 1;
+            status = TestCase03(driver);
+            if (status) {
+            passedTests += 1;
+            }
 
-            // System.out.println("");
+            System.out.println("");
 
             // Execute Test Case 4
             totalTests += 1;
@@ -494,31 +494,31 @@ public class QkartSanity {
             System.out.println("");
 
             // Execute Test Case 5
-            // totalTests += 1;
-            // status = TestCase05(driver);
-            // if (status) {
-            // passedTests += 1;
-            // }
+            totalTests += 1;
+            status = TestCase05(driver);
+            if (status) {
+            passedTests += 1;
+            }
 
-            // System.out.println("");
+            System.out.println("");
 
             // Execute Test Case 6
-            // totalTests += 1;
-            // status = TestCase06(driver);
-            // if (status) {
-            // passedTests += 1;
-            // }
+            totalTests += 1;
+            status = TestCase06(driver);
+            if (status) {
+            passedTests += 1;
+            }
 
-            // System.out.println("");
+            System.out.println("");
 
             //  Execute Test Case 7
-            // totalTests += 1;
-            // status = TestCase07(driver);
-            // if (status) {
-            // passedTests += 1;
-            // }
+            totalTests += 1;
+            status = TestCase07(driver);
+            if (status) {
+            passedTests += 1;
+            }
 
-            // System.out.println("");
+            System.out.println("");
 
 
         } catch (Exception e) {
